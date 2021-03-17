269eb12f55ee017e2f99680744cf38884c704ed5
Rodrigo Sobral <rodrigosobral@sapo.pt>
2021-03-17 20:09:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment_2021_SD
+ 
---
+++ src/RMIServer.java
@@ -0,0 +1,5 @@
+public class RMIServer {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
