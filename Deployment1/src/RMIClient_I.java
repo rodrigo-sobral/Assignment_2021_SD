07d40af8884e4a2a1fb95be02952df23b427714a
Rodrigo Sobral <rodrigosobral@sapo.pt>
2021-05-04 14:39:49 +0100

    Starting Stage 2

--- Deployment1/src/RMIClient_I.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-
-
-public interface RMIClient_I extends Remote {
-    public boolean setNewServer(String new_server_ip) throws RemoteException;
-    public String ping() throws RemoteException;
-}
